f41d9807c4472c3f0e36c197ea5b40938131136e
Surpreet Mundra <surpreet.mundra@gmail.com>
2021-05-11 11:22:54 -0400

    adding fetch request to check we can pull data from our api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+  componentDidMount () {
+    fetch ('http://localhost:3000/api/v1/books', {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
   render () {
     return (
       <div className="App">
