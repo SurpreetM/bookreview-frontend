566e09ac30830fe277bbe9aaff98536e86a431ef
Surpreet Mundra <surpreet.mundra@gmail.com>
2021-05-12 10:45:50 -0400

    importing the redux, redux-react and redux-thunk libraries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 class App extends React.Component {
 
+  // Testing fetch request 
   componentDidMount () {
-    fetch ('http://localhost:3000/api/v1/books/1/reviews', {
+    fetch ('http://localhost:3000/api/v1/books', {
       method: 'GET'
     })
     .then(response => response.json())
