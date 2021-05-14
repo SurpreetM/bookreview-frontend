65b1cdd87f7cff85b33ba6277338c95ad0ac5e2b
Surpreet Mundra <surpreet.mundra@gmail.com>
2021-05-14 10:18:36 -0400

    refactored booklist as a functional component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,17 +1,30 @@
 import React from 'react'
 
-class BookList extends React.Component {
 
-    BookList = () => { return this.props.books.map (book => 
-        <li>{book.title}</li>)
-    }
-    render() {
-        return (
-            <div>
-                {this.BookList()}
-            </div>
-        )
-    }
+const BookList = (props) => {
+
+    return (
+        <div>
+            {props.books.map (book => 
+            <li key= {book.id}> {book.title} {book.average_rating}/10</li>)}         
+        </div>
+    )
+    
 }
+
+
+//class BookList extends React.Component {
+
+//    BookList = () => { return this.props.books.map (book => 
+//        <li key={book.id}>Title: {book.title} {book.average_rating}/10</li>)
+//    }
+//    render() {
+//        return (
+//            <div>
+//                {this.BookList()}
+//            </div>
+//        )
+//    }
+//}
 
 export default BookList
