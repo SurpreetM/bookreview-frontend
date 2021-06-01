cda4298b48f90b06efef1ec50e6c00353f744839
Surpreet Mundra <surpreet.mundra@gmail.com>
2021-06-01 08:26:48 -0400

    final edits

--- src/actions/addBook.js
+++ src/actions/addBook.js
@@ -5,7 +5,7 @@ export const addBook = (data) => {
     console.log(data)
     
     return (dispatch) => {
-      fetch('http://localhost:3000/api/v1/books', {
+      return fetch('http://localhost:3000/api/v1/books', {
         headers: {
           'Content-Type': 'application/json',
           'Accept': 'application/json'
